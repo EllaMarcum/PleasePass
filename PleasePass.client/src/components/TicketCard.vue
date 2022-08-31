ebc12b3abb998f2fc26d1e2c04daca700c433c62
Ellamarcum <Ecaroline_29@icloud.com>
2022-08-30 20:05:14 -0600

    final commit

---
+++ PleasePass.client/src/components/TicketCard.vue
@@ -0,0 +1,26 @@
+<!-- <template>
+  <div class="bg-dark p-2 rounded"></div>
+  <img class="img-fluid" :src="ticket.profile?.picture" alt="">
+  <h4 class="text-center">{{  ticket.event.title  }}</h4>
+  <button class="btn btn-danger" @click="removeTicket">Cancel Ticket</button>
+</template>
+<script>
+import { ticketService } from '../services/TicketService';
+import { logger } from '../utils/Logger';
+import Pop from '../utils/Pop';
+export default {
+  props: { ticket: { type: Object, required: true } },
+  setup() {
+    return {
+      async removeTicket(ticketId) {
+        try {
+          await ticketService.removeTicket(ticketId)
+        } catch (error) {
+          logger.log('[removing ticket]', error)
+          Pop.error(error)
+        }
+      }
+    }
+  }
+}
+</script> -->
